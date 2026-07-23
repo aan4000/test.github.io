c9dff6c220055e9e164ccc48e951a18790243551
Aandev <alcantaraalberto805@gmail.com>
2026-07-23 00:40:16 -0400

    Add files via upload

---
+++ scrips/script.js
@@ -0,0 +1,53 @@
+const respNav = document.getElementById("respNav");
+const respMenu = document.getElementById("resMenu")
+
+// respNav.style.display = "none"
+
+respMenu.addEventListener("click",()=>{
+
+    respNav.classList.toggle("showMe")
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+const reset = document.getElementById("reset").addEventListener("click",()=>{
+
+   const confirmacion =  confirm( "quieres borrar este contenido?");
+
+ 
+
+});
+
+// FORMULARIO DE CONTACTO
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_ud00jsu';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('SU MENSAJE FUE ENVIADO!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
+
